da8fb85f34e260126658606aefd3d38929235ab3
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-09 03:47:55 +0900

    Add

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("hello textvid-api");
+use lambda_http::{run, service_fn, Error, IntoResponse, Request, RequestExt, Response};
+
+async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body("Hello AWS Lambda HTTP request")
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .without_time()
+        .init();
+    run(service_fn(function_handler)).await
 }
