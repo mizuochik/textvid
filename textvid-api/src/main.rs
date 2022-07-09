187748ad98730ce5f42899a432f8ef30fd09fc6a
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-09 22:42:04 +0900

    Introduce anyhow

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,22 +1,23 @@
+use anyhow::{self, Context};
 use axum::{routing, Router};
 use std::net::SocketAddr;
-
 use tokio::signal::unix::{self, SignalKind};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let app = Router::new().route("/", routing::get(root));
-    run_server(app).await.unwrap();
+    run_server(app).await
 }
 
-async fn run_server(r: Router) -> hyper::Result<()> {
+async fn run_server(r: Router) -> anyhow::Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::info!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(r.into_make_service())
         .with_graceful_shutdown(wait_shutdown())
         .await
+        .context("run_server")
 }
 
 async fn wait_shutdown() {
