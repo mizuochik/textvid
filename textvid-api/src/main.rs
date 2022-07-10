29cccedca92a99d7954e18313f9a65d53bb7d8f1
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-11 01:15:58 +0900

    Introduce axum

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,77 +1,19 @@
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-use std::env;
-use std::future::Future;
-use std::{convert::Infallible, sync::Arc};
+use anyhow::Context;
+use axum::{routing, Router, Server};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let h = Handler {};
-    if env::var("AWS_LAMBDA_RUNTIME_API").is_ok() {
-        run_lambda(handler).await?;
-    } else {
-        run_server(h).await?;
-    }
-    Ok(())
-}
-
-async fn run_lambda<F, R, T>(f: F) -> anyhow::Result<()>
-where
-    R: lambda_http::IntoResponse,
-    F: Fn(lambda_http::Request) -> T,
-    T: Future<Output = Result<R, lambda_http::Error>>,
-{
-    lambda_http::run(lambda_http::service_fn(f))
+    let app = Router::new().route("/", routing::get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("Listening on {}", addr);
+    Server::bind(&addr)
+        .serve(app.into_make_service())
         .await
-        .map_err(|e| anyhow::anyhow!("lambda: {}", e))
+        .context("serve")
 }
 
-async fn run_server(h: Handler) -> anyhow::Result<()> {
-    let addr = ([127, 0, 0, 1], 3000).into();
-    let h = Arc::new(h);
-    let make_svc = make_service_fn(|_| {
-        let h = h.clone();
-        async {
-            Ok::<_, Infallible>(service_fn(move |_: Request<Body>| {
-                let h = h.clone();
-                async move {
-                    let req = Request::builder().body(lambda_http::Body::Empty)?;
-                    let res = h.handle(req).await.map_err(|e| anyhow::anyhow!("lambda: {}", e))?;
-                    let res = Response::builder()
-                        .status(200)
-                        .body(Body::from("Hello Textvid"))?;
-                    Ok::<_, anyhow::Error>(res)
-                }
-            }))
-        }
-    });
-    let server = Server::bind(&addr).serve(make_svc);
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
-    Ok(())
-}
-
-pub struct Handler {}
-
-impl Handler {
-    pub async fn handle(
-        &self,
-        _req: lambda_http::Request,
-    ) -> Result<impl lambda_http::IntoResponse, lambda_http::Error> {
-        Ok(lambda_http::Response::builder()
-            .status(200)
-            .body("hoge")
-            .unwrap())
-    }
-}
-
-async fn handler(
-    _req: lambda_http::Request,
-) -> Result<impl lambda_http::IntoResponse, lambda_http::Error> {
-    Ok(lambda_http::Response::builder()
-        .status(200)
-        .body("hoge")
-        .unwrap())
+async fn root() -> &'static str {
+    "hello world"
 }
