541974657b00c011adacccb3754f1539925172b4
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-11 01:46:39 +0900

    Lambda mode

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,8 +1,15 @@
+use anyhow::anyhow;
+
 mod di;
 mod infra;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    di::DI::new().server.serve().await
+    let d = di::DI::new();
+    if std::env::var("AWS_LAMBDA_RUNTIME_API").is_ok() {
+        Err(anyhow!("not implemented"))
+    } else {
+        d.server.serve().await
+    }
 }
