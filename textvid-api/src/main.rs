43bad945a3571543c835d4aa3610e2ae988f9500
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-10 22:52:36 +0900

    Fix

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,16 +1,17 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use std::convert::Infallible;
 use std::env;
 use std::future::Future;
+use std::{convert::Infallible, sync::Arc};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
+    let h = Handler {};
     if env::var("AWS_LAMBDA_RUNTIME_API").is_ok() {
         run_lambda(handler).await?;
     } else {
-        run_server(handler).await?;
+        run_server(handler, h).await?;
     }
     Ok(())
 }
@@ -26,15 +27,12 @@ where
         .map_err(|e| anyhow::anyhow!("lambda: {}", e))
 }
 
-async fn run_server<F, R, T>(_f: F) -> anyhow::Result<()>
-where
-    R: lambda_http::IntoResponse,
-    F: Fn(lambda_http::Request) -> T,
-    T: Future<Output = Result<R, lambda_http::Error>>,
-{
+async fn run_server(h: Handler) -> anyhow::Result<()> {
     let addr = ([127, 0, 0, 1], 3000).into();
-    let make_svc = make_service_fn(|_| async move {
-        Ok::<_, Infallible>(service_fn(move |_: Request<Body>| async move {
+    let make_svc = make_service_fn(|_| async {
+        Ok::<_, Infallible>(service_fn(|_: Request<Body>| async {
+            let req = Request::builder().body(lambda_http::Body::Empty)?;
+            h.handle(req);
             let res = Response::builder()
                 .status(200)
                 .body(Body::from("Hello Textvid"))?;
@@ -46,6 +44,20 @@ where
         eprintln!("server error: {}", e);
     }
     Ok(())
+}
+
+pub struct Handler {}
+
+impl Handler {
+    pub async fn handle(
+        &self,
+        _req: lambda_http::Request,
+    ) -> Result<impl lambda_http::IntoResponse, lambda_http::Error> {
+        Ok(lambda_http::Response::builder()
+            .status(200)
+            .body("hoge")
+            .unwrap())
+    }
 }
 
 async fn handler(
