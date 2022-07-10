7af25f2724c213dd1b985caa0a438fb63eb9ca1f
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-10 21:27:40 +0900

    Extract

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,13 +1,24 @@
+use std::future::Future;
+
 use anyhow;
 use lambda_http;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    lambda_http::run(lambda_http::service_fn(handler))
-        .await
-        .map_err(|e| anyhow::anyhow!("lambda: {}", e))?;
+    run_lambda(handler).await?;
     Ok(())
+}
+
+async fn run_lambda<F, R, T>(f: F) -> anyhow::Result<()>
+where
+    R: lambda_http::IntoResponse,
+    F: Fn(lambda_http::Request) -> T,
+    T: Future<Output = Result<R, lambda_http::Error>>,
+{
+    lambda_http::run(lambda_http::service_fn(f))
+        .await
+        .map_err(|e| anyhow::anyhow!("lambda: {}", e))
 }
 
 async fn handler(
