0691b972b97f7ef32936757aa2bd05f393189d81
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-07-10 21:00:27 +0900

    Convert to lambda

--- textvid-api/src/main.rs
+++ textvid-api/src/main.rs
@@ -1,14 +1,49 @@
 use anyhow::{self, Context};
 use axum::{routing, Router};
-use std::net::SocketAddr;
+use http::Request;
+use lambda_http::{tower::ServiceExt, Service};
+use std::{net::SocketAddr, sync::Arc};
 
-use tokio::signal::unix::{self, SignalKind};
+use tokio::{
+    signal::unix::{self, SignalKind},
+    sync::Mutex,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let app = Router::new().route("/", routing::get(root));
+    let app: Router = Router::new().route("/", routing::get(root));
     run_server(app).await
+}
+
+async fn run_lambda(r: Router) -> anyhow::Result<()> {
+    let rr = Mutex::new(r);
+    lambda_http::run(lambda_http::service_fn(
+        |req: Request<lambda_http::Body>| async {
+            let req = Arc::new(req).clone();
+            let ax_body = match req.body() {
+                lambda_http::Body::Text(t) => axum::body::Body::from(t.clone()),
+                _ => axum::body::Body::empty(),
+            };
+            let ax_req = Request::builder()
+                .method(req.method().as_str())
+                .uri(req.uri())
+                .header(
+                    "Content-Type",
+                    req.headers().get("Content-Type").context("content-type")?,
+                )
+                .body(ax_body)?;
+            let ax_res = rr.lock().await.call(ax_req).await?;
+            let b = ax_res.body();
+            let lam_res = lambda_http::Response::builder()
+                .status(ax_res.status())
+                .body(lambda_http::Body::Text(String::from("hello")))?;
+            Ok(lam_res)
+        },
+    ))
+    .await
+    .map_err(|err| anyhow::anyhow!("{}", err))?;
+    Ok(())
 }
 
 async fn run_server(r: Router) -> anyhow::Result<()> {
